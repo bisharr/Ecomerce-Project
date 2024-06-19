17581afe6d886526a50c7c62b1bc9a467ee3ec13
bisharr <bishar995@gmail.com>
2024-06-19 18:45:36 +0300

    creating Rootes and pages

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "remixicon/fonts/remixicon.css";
-import "bootstrap/dist/css/bootstrap.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import 'remixicon/fonts/remixicon.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
-import App from "./App";
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
---
+++ src/routers/Routers.js
@@ -0,0 +1,26 @@
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+import Home from '../pages/Home';
+import Cart from '../pages/Cart';
+import Login from '../pages/Login';
+import ProductDetails from '../pages/ProductDetails';
+import Signup from '../pages/Signup';
+import Shop from '../pages/Shop';
+import Checkout from '../pages/Checkout';
+
+const Routers = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<Navigate to='home' />} />
+      <Route path='home' element={<Home />} />
+      <Route path='shop' element={<Shop />} />
+      <Route path='shop/:id' element={<ProductDetails />} />
+      <Route path='cart' element={<Cart />} />
+      <Route path='checkout' element={<Checkout />} />
+      <Route path='login' element={<Login />} />
+      <Route path='sigin' element={<Signup />} />
+    </Routes>
+  );
+};
+
+export default Routers;
