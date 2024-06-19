7c82a9eb3d19c71a7107e6809ec45d7dbc8e7742
bisharr <bishar995@gmail.com>
2024-06-20 01:54:06 +0300

    header completed

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,83 @@
 import React from 'react';
+import './header.css';
+import { NavLink } from 'react-router-dom';
+
+import { Container, Row } from 'reactstrap';
+import logo from '../../assets/images/eco-logo.png';
+import userIcon from '../../assets/images/user-icon.png';
+import { motion } from 'framer-motion';
+
+const nav_links = [
+  {
+    path: 'home',
+    display: 'Home',
+  },
+  {
+    path: 'shop',
+    display: 'Shop',
+  },
+  {
+    path: 'cart',
+    display: 'Cart',
+  },
+];
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className='header'>
+      <Container>
+        <div className='nav_wrapper'>
+          <div className='logo'>
+            <img src={logo} alt='logo' />
+            <div>
+              <h1>Multimarti</h1>
+            </div>
+          </div>
+          <div className='navigation'>
+            <ul className='menu'>
+              {nav_links.map((item, index) => (
+                <li className='nav_item' key={index}>
+                  {' '}
+                  <NavLink
+                    to={item.path}
+                    className={(navClass) =>
+                      navClass.isActive ? 'nav_active' : ''
+                    }
+                  >
+                    {item.display}
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className='nav__icons'>
+            <span className='fav_icon'>
+              <i class='ri-heart-line'></i>
+              <span className='badge'>1</span>
+            </span>
+            <span className='cart_icon'>
+              <i class='ri-shopping-bag-line'></i>
+              <span className='badge'>1</span>
+            </span>
+            <span>
+              <motion.img
+                whileTap={{ scale: 1.2 }}
+                src={userIcon}
+                alt='user-icon'
+              />
+            </span>
+          </div>
+
+          <div className='mobile_menu'>
+            <span>
+              <i class='ri-menu-line'></i>
+            </span>
+          </div>
+        </div>
+        <Row></Row>
+      </Container>
+    </header>
+  );
 };
 
 export default Header;
