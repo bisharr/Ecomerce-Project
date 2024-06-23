76a2490482a891190cdd7cb1afe580d63d132774
bisharr <bishar995@gmail.com>
2024-06-23 09:09:07 +0300

    product details

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
+import Helmet from '../components/helmet/Helmet';
+import products from '../assets/data/products';
+import CommoSection from '../components/UI/CommoSection';
+import { useParams } from 'react-router-dom';
 
 const ProductDetails = () => {
-  return <div>ProductDetails</div>;
+  const { id } = useParams();
+  const product = products.find((item) => item.id === id);
+  const { imgUrl } = product;
+
+  return (
+    <Helmet title={'ProductDetails'}>
+      <CommoSection />
+      <section className='pt-0'>
+        <Container>
+          <Row>
+            <Col lg='6'>
+              <p>Heloo</p>
+            </Col>
+            <Col lg='6'></Col>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
 };
 
 export default ProductDetails;
--- src/routers/Routers.js
+++ src/routers/Routers.js
@@ -14,11 +14,11 @@ const Routers = () => {
       <Route path='/' element={<Navigate to='home' />} />
       <Route path='home' element={<Home />} />
       <Route path='shop' element={<Shop />} />
-      <Route path='shop/:id' element={<ProductDetails />} />
       <Route path='cart' element={<Cart />} />
       <Route path='checkout' element={<Checkout />} />
       <Route path='login' element={<Login />} />
       <Route path='sigin' element={<Signup />} />
+      <Route path='details' element={<ProductDetails />} />
     </Routes>
   );
 };
