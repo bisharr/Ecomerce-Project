0384bf05bfce1ef22aa592e62a68ad786309f891
bisharr <bishar995@gmail.com>
2024-06-25 07:50:34 +0300

    completed Checkout page

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,7 +1,78 @@
 import React from 'react';
+import { Container, Col, Row, FormGroup } from 'reactstrap';
+import Helmet from '../components/helmet/Helmet';
+import CommoSection from '../components/UI/CommoSection';
+import '../styles/checkout.css';
+import { useSelector } from 'react-redux';
 
 const Checkout = () => {
-  return <div>Checkout</div>;
+  const totalQty = useSelector((state) => state.cart.totalQuantity);
+  const totalAmount = useSelector((state) => state.cart.totalAmount);
+  return (
+    <Helmet title='Checkout'>
+      <CommoSection title='Checkout' />
+      <section>
+        <Container>
+          <Row>
+            <Col lg='8'>
+              <h6 className='mb-4 fw-bold'>Billing Information</h6>
+              <form className='billing__form'>
+                <FormGroup className='form__group'>
+                  <input type='text' placeholder='Enter your Name' />
+                </FormGroup>
+                <FormGroup className='form__group'>
+                  <input type='email' placeholder='Enter your email' />
+                </FormGroup>
+
+                <FormGroup className='form__group'>
+                  <input type='number' placeholder=' phone number' />
+                </FormGroup>
+
+                <FormGroup className='form__group'>
+                  <input type='text' placeholder='street address' />
+                </FormGroup>
+
+                <FormGroup className='form__group'>
+                  <input type='text' placeholder='City' />
+                </FormGroup>
+                <FormGroup className='form__group'>
+                  <input type='text' placeholder='Postol code' />
+                </FormGroup>
+                <FormGroup className='form__group'>
+                  <input type='text' placeholder='Country' />
+                </FormGroup>
+              </form>
+            </Col>
+            <Col lg='4'>
+              <div className='checkout__cart'>
+                <h6>
+                  Total Qty: <span>{totalQty} items</span>
+                </h6>
+                <h6>
+                  Subtotal: <span>${totalAmount}</span>
+                </h6>
+                <h6>
+                  <span>
+                    {' '}
+                    Shipping: <br />
+                    free Shipping
+                  </span>{' '}
+                  <span>$0</span>
+                </h6>
+
+                <h4>
+                  Total Cost <span>${totalAmount}</span>
+                </h4>
+                <button className='buy_btn auth__btn w-100'>
+                  Place an order
+                </button>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
 };
 
 export default Checkout;
