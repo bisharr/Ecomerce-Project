36f1b4b9dc70e0a52a21951f8b667b7cd187f2e7
bisharr <bishar995@gmail.com>
2024-06-30 19:00:44 +0300

    implemented delete btn

--- src/admin/AllProducts.jsx
+++ src/admin/AllProducts.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import { Container, Row, Col } from 'reactstrap';
+import { db } from '../firebase.config';
+import { doc, deleteDoc } from 'firebase/firestore';
+import { toast } from 'react-toastify';
 
 import useGetData from '../custom-hooks/useGetData';
 
 function AllProducts() {
-  const { data: productsData } = useGetData('products');
+  const { data: productsData, loading } = useGetData('products');
+
+  const deleteProduct = async (id) => {
+    await deleteDoc(doc(db, 'products', id));
+    toast.success('product deleted!');
+  };
 
   return (
     <section>
@@ -22,20 +30,30 @@ function AllProducts() {
                 </tr>
               </thead>
               <tbody>
-                {productsData.map((item) => (
-                  <tr key={item.id}>
-                    <td>
-                      <img src={item.imgUrl} alt='img' />
-                    </td>
-                    <td>{item.title}</td>
-                    <td>{item.category}</td>
-                    <td>{item.price}$</td>
-                    <td>
-                      {' '}
-                      <button className='btn btn-danger'>Delete</button>
-                    </td>
-                  </tr>
-                ))}
+                {loading ? (
+                  <h4 className='py-5 text-center fw-bold'>Loading....</h4>
+                ) : (
+                  productsData.map((item, index) => (
+                    <tr key={item.id || index}>
+                      <td>
+                        <img src={item.imgUrl} alt='img' />
+                      </td>
+                      <td>{item.title}</td>
+                      <td>{item.category}</td>
+                      <td>{item.price}$</td>
+                      <td>
+                        <button
+                          onClick={() => {
+                            deleteProduct(item.id);
+                          }}
+                          className='btn btn-danger'
+                        >
+                          Delete
+                        </button>
+                      </td>
+                    </tr>
+                  ))
+                )}
               </tbody>
             </table>
           </Col>
--- src/custom-hooks/useGetData.js
+++ src/custom-hooks/useGetData.js
@@ -4,18 +4,20 @@ import { collection, getDocs } from 'firebase/firestore';
 
 const useGetData = (collectionName) => {
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
   const collectionRef = collection(db, collectionName);
 
   useEffect(() => {
     const getData = async () => {
       const data = await getDocs(collectionRef);
 
-      setData(data.docs.map((doc) => ({ ...doc.data() })));
+      setData(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      setLoading(false);
     };
     getData();
   }, []);
 
-  return { data };
+  return { data, loading };
 };
 
 export default useGetData;
